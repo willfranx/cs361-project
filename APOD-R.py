ed603d08406afdfc10a2917d6fc18395f964deb7
William <wrightfranklin907@gmail.com>
2024-04-20 10:50:59 -0800

    Added some initial dialogs

--- APOD-R.py
+++ APOD-R.py
@@ -1,7 +1,33 @@
 import tkinter as tk
+from tkinter import messagebox, filedialog
+
+def show_info_dialog():
+    messagebox.showinfo("Information", "This is an informational dialog!")
+
+def select_directory():
+    directory = filedialog.askdirectory()  # open the directory chooser dialog
+    print(f"Selected directory: {directory}")  # print the selected directory
 
 window = tk.Tk()
-label = tk.Label(text="CS361 rocks!")
+label = tk.Label(text="NASA Astronomy Picture of the Day (APOD) Randomizer!")
 label.pack()
+
+info_button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    command=show_info_dialog  # function to call when the info button is clicked
+)
+info_button.pack()
+
+dir_button = tk.Button(
+    text="Select directory",
+    width=25,
+    height=5,
+    command=select_directory  # function to call when the directory button is clicked
+)
+dir_button.pack()
+
+show_info_dialog()  # call the function here to show the dialog on startup
 
 window.mainloop()
